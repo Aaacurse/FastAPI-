ff6410f60d669652c26b791d66a8f24bdf66621e
Aaacurse <rajputaakarsh2004@gmail.com>
2026-04-19 22:57:43 +0530

    Path parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException
 import json
 
 
@@ -25,3 +25,13 @@ def about():
 def view():
     data=load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str=Path(...,description="ID of the patient in DB",examples="P001")):
+    #load all patients
+    data=load_data()
+    if patient_id in data:
+        return data[patient_id]
+    
+    raise HTTPException(status_code=404,detail="Patient not found")
+    
