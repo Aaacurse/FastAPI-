1cb2060e5240f49bd97f365e81eb48f30e6d7222
Aaacurse <rajputaakarsh2004@gmail.com>
2026-04-22 23:14:48 +0530

    Post Method and Pydantic Object

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 from fastapi import FastAPI,Path,HTTPException,Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel,Field,computed_field
 import json
+from typing import Annotated,List,Literal
 
+class Patient(BaseModel):
+    id:Annotated[str,Field(...,description='Id of the patient',examples=['P001'])]
+    name:Annotated[str,Field(...)]
+    city:Annotated[str,Field(...)]
+    age:Annotated[int,Field(...,gt=0,le=120)]
+    gender:Annotated[Literal["Male","Female","Others"],Field(...)]
+    height:Annotated[float,Field(...)]
+    weight:Annotated[float,Field(...)]
+    
+    @computed_field
+    @property
+    def bmi(self)->float:
+        bmi=round(self.weight/(self.height**2))
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self)->str:
+        if self.bmi<18.5:
+            return "Underweight"
+        elif self.bmi<25:
+            return "Normal"
+        elif self.bmi<30:
+            return "Overweight"
+        else:
+            return "Obese"
+    
 
 app=FastAPI()
 
@@ -10,6 +40,10 @@ def load_data():
         
     return file
 
+
+def save_data(data):
+    with open('Patient.json','w') as f:
+        json.dump(data,f)
 
 
 @app.get("/")
@@ -49,4 +83,17 @@ def sort_patients(sort_by:str=Query(...,description="Sort on basis of height,wei
     sorted_data=sorted(data.values(),key=lambda x: x.get(sort_by,0),reverse=sort_order)
     
     return sorted_data
+
+@app.post('/create')
+def create_patient(patient:Patient):
+    #load existing data
+    data=load_data()
+    #check if patient exists
+    if patient.id in data:
+        raise HTTPException(status_code=400,detail="Patient already exits")
+    #new patient add to db
+    data[patient.id]=patient.model_dump(exclude=['id'])
+    #save data
+    save_data(data=data)
     
+    return JSONResponse(status_code=201,content={"message":"patient created successfully"})
